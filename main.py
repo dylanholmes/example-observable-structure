df91b4c2bbd0a01075bd62f2c73e1c3a95facca7
dylanholmes <4370153+dylanholmes@users.noreply.github.com>
2024-07-01 15:47:26 -0700

    Add GriptapeCloudEventListenerDriver

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from griptape.drivers import GriptapeCloudObservabilityDriver
+from griptape.drivers import GriptapeCloudEventListenerDriver, GriptapeCloudObservabilityDriver
+from griptape.events import EventListener
 from griptape.rules import Rule
 from griptape.structures import Agent
 from griptape.observability import Observability
 
 observability_driver = GriptapeCloudObservabilityDriver(service_name="example-observable-structure")
+event_listener_driver = GriptapeCloudEventListenerDriver()
 
 with Observability(observability_driver=observability_driver):
-    agent = Agent(rules=[Rule("Output one word")])
+    agent = Agent(
+        rules=[Rule("Output one word")],
+        event_listeners=[EventListener(driver=event_listener_driver)]
+    )
     agent.run("Name an animal")
 
