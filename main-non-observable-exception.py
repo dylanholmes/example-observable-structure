98ed94d587e2dfc872c8fe7ba4d7b3e9b879fd84
dylanholmes <4370153+dylanholmes@users.noreply.github.com>
2024-07-09 07:14:30 -0700

    Change griptape branch

--- main-non-observable-exception.py
+++ main-non-observable-exception.py
@@ -1,3 +1,19 @@
+import os
+
+from griptape.drivers import GriptapeCloudEventListenerDriver, GriptapeCloudObservabilityDriver
+from griptape.events import EventListener
+from griptape.rules import Rule
+from griptape.structures import Agent
+from griptape.observability import Observability
+
+observability_driver = GriptapeCloudObservabilityDriver(service_name="example-observable-structure")
+event_listener_driver = GriptapeCloudEventListenerDriver(api_key=os.environ["GT_CLOUD_API_KEY"])
+
+with Observability(observability_driver=observability_driver):
+    agent = Agent(
+        rules=[Rule("Output one word")],
+        event_listeners=[EventListener(driver=event_listener_driver)]
+    )
+    agent.run("Name an animal")
 
 raise Exception("Non-observable exception")
-
